27c0e3d8ffab30a876b830992b9076b1295d877b
ALBIN JACOB <ajrockies@gmail.com>
2019-01-08 16:27:57 +0530

    web api base code
    .netcore

---
+++ service/service/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using service.Models;
+
+namespace service.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<List<Employee>> GetEmployees()
+        {
+            var dbContext = new DbBaseContext();
+            return dbContext.Employee.ToList();
+            //List<Employee> list = new List<Employee>();
+            //return list;
+        }
+
+        // GET api/values
+        [HttpPost]
+        public ActionResult<Employee> SaveChanges()
+        {
+            Employee employee = new Employee();
+            var dbContext = new DbBaseContext();
+            dbContext.Employee.Add(employee);
+            dbContext.SaveChanges();
+
+            return employee;
+        }
+    }
+}
---
+++ service/service/DbContext/DbBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace service
+{
+    public class DbBaseContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=KODEVALBINJ\EAUDIT;Database=EmployeeDB;User Id=sa;Password=Password!;");
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
